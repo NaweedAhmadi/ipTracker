d4c4990a48e6bafb79b1f015d03655a569d84f46
Naweed Ahmadi <Naweed-Ahmadi@scusd.edu>
2022-11-05 14:55:48 -0700

    v1 - ipTracker app

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+var searchIP = "";
+
+app.get("/", function(req, res) {
+
+  // First API - With the help of this API we get the Public IP address
+  const ipURL = "https://api.ipify.org?format=json"
+
+  https.get(ipURL, function(response) {
+    response.on("data", function(data) {
+      const userIP = JSON.parse(data);
+      const ipAddress = userIP.ip;
+
+      var currentIP = "";
+
+      if (searchIP == "") {
+        currentIP = ipAddress;
+      } else {
+        currentIP = searchIP;
+      }
+
+      // Second API - With the help of this API we get the location of the IP address
+      const locationURL = "https://geo.ipify.org/api/v2/country,city?apiKey=at_MLyNpOUdp9z8XRoGyj3wIZvvlrtjP&ipAddress=" + currentIP;
+      // at_MLyNpOUdp9z8XRoGyj3wIZvvlrtjP
+
+      https.get(locationURL, function(response) {
+        response.on("data", function(data) {
+          const ipLocation = JSON.parse(data);
+          const myIP = ipLocation.ip;
+          const region = ipLocation.location.region;
+          const country = ipLocation.location.country;
+          const zip = ipLocation.location.postalCode;
+          const timezone = ipLocation.location.timezone;
+          const isp = ipLocation.isp;
+          const lat = ipLocation.location.lat;
+          const lng = ipLocation.location.lng;
+
+          res.render("index", {
+            ip: myIP,
+            region: region,
+            country: country,
+            zip: zip,
+            timezone: timezone,
+            isp: isp,
+            lat: lat,
+            lng: lng
+          })
+        })
+      })
+    })
+  })
+})
+
+app.post("/", function(req, res) {
+  searchIP = req.body.searchIP;
+  res.redirect("/");
+})
+
+app.listen(3000, function(req, res) {
+  console.log("The server is running on port 3000.");
+})
